a67ea68c834c3b2389aebc0eef5615e027cd0217
Jacob Gelbman <gelbman@gmail.com>
2014-07-20 17:45:07 -0400

    Add left and right links on gallery images

--- themes/default/gallery.js
+++ themes/default/gallery.js
@@ -14,15 +14,51 @@ function viewImage (index) {
     var thumbLink = thumbLinks.eq(index);
     if (!imageViewer)
         createImageViewer();
-    imageViewer.empty();
+    imageViewer.find(".image-viewer-main").empty();
+    imageViewer.find(".image-viewer-info").empty();
     var img = $("<img></img>");
     img.attr("src", thumbLink.attr("href"));
-    imageViewer.append(img);
+    imageViewer.find(".image-viewer-main").append(img);
+    if (index > 0) {
+        var prev = $("<a href=\"#\">←</a>");
+        prev.on("click", function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            viewImage(index - 1);
+        });
+        imageViewer.find(".image-viewer-info").append(prev);
+    }
+    if (index < thumbLinks.length - 1) {
+        var next = $("<a href=\"#\">→</a>");
+        next.on("click", function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            viewImage(index + 1);
+        });
+        if (prev) {
+            imageViewer.find(".image-viewer-info").append("&nbsp;&nbsp;&nbsp;&nbsp;");
+        }
+        imageViewer.find(".image-viewer-info").append(next);
+    }
+    var span = $("<span></style>");
+    span.css("float", "right");
+    var close = $("<a href=\"#\">close</a>");
+    close.on("click", function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        imageViewer.hide();
+    });
+    span.append(close);
+    imageViewer.find(".image-viewer-info").append(span);
     imageViewer.show();
 }
 
 function createImageViewer () {
-    imageViewer = $("<div class=\"image-viewer\"></div>");
+    imageViewer = $("<div></div>");
+    imageViewer.attr("class", "image-viewer");
+    imageViewer.append("<table><tr><td></td></tr><tr><td></td></tr></table>");
+    imageViewer.find("td").eq(0).attr("class", "image-viewer-main");
+    imageViewer.find("td").eq(1).attr("class", "image-viewer-info");
     imageViewer.hide();
     imageViewer.appendTo("body");
     imageViewer.click(function (event) {
